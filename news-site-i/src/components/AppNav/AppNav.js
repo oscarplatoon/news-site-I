001934a5f809c23802a47f7261468c8a9d76de0c
Marc Stanley <marcson909@gmail.com>
2021-07-21 11:00:38 -0500

    App Nav complete up to Functional tasks

--- news-site-i/src/components/AppNav/AppNav.js
+++ news-site-i/src/components/AppNav/AppNav.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class AppNav extends Component {
 
+    displayNavItems = () => {
+    return this.props.navItems.map((item, index) => {
+      return <a href="#" key={index} value={item['value']} onClick={() => this.props.handleNavClick(item['value'])}>{item['label']}</a>
+    })
+   }
+
   render() {
     return (
       <nav>
+        {this.displayNavItems()}
       </nav>
     )
   }
