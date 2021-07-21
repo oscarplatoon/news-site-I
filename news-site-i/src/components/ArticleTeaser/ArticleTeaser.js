81f987dc28d398ba41d67a003041fd85c8fd5119
AlexanderS68 <80540025+AlexanderS68@users.noreply.github.com>
2021-07-21 14:57:06 -0400

    article teaser

---
+++ news-site-i/src/components/ArticleTeaser/ArticleTeaser.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ArticleTeaser extends Component {
+  render() {
+    return (
+      <div>
+        <a href='#' onClick={ () => this.props.handleTitleClick(this.props.id) }> { this.props.title }</a>
+        <p>{ this.props.created_date}</p>
+      </div>
+    )
+  }
+}
+
+export default ArticleTeaser;
