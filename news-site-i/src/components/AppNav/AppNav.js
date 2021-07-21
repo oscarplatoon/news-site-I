57ed75354f69ab5c4f2ca12f9971155acbd84bc3
kylewgunter <kylewgunter@gmail.com>
2021-07-21 17:11:03 -0500

    kyle wip 2.0

--- news-site-i/src/components/AppNav/AppNav.js
+++ news-site-i/src/components/AppNav/AppNav.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class AppNav extends Component {
+ 
+  // anon function that calls on each element in the array and maps
+  returnNavItems = () => {
+    let values = this.props.navItems.map((item, index) => {
+      return (
+        <a key={`nav-${index}`} onClick={ () => this.props.handleNavClick(item.value) } href="#">{ item.label }&nbsp;|</a>
+      )
+    })
+    return values
+  }
 
   render() {
     return (
       <nav>
+      { this.returnNavItems() }
       </nav>
     )
   }
