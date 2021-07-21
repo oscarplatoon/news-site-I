5e23f3361ccbbda1dcec854fa6a4962f1d5d5228
Marc Stanley <marcson909@gmail.com>
2021-07-21 10:40:48 -0500

    Article component complete

--- news-site-i/src/components/Article/Article.js
+++ news-site-i/src/components/Article/Article.js
@@ -11,6 +11,13 @@ class Article extends Component {
   render() {  
     return (
       <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.created_date}</p>
+        { this.props.byline &&
+        <h2>{this.props.byline}</h2>
+        }
+        <img src={ this.props.image } alt="img"></img>
+        <p>{this.props.abstract}</p>
       </div>
     )
   }
