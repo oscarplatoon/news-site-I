6d100ad477e8ddf230af747dd4e5f884aab7a821
Ankur Shah <ankur@codeplatoon.org>
2021-07-21 11:12:13 -0500

    completed part III

--- news-site-i/src/components/AppNav/AppNav.js
+++ news-site-i/src/components/AppNav/AppNav.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
+// 1. Map through this.props.navItems to create an array of <a> elements. The objects within this.props.navItems look something like this:
+// label => text to show
+// value => value to pass to the handler function
+
 class AppNav extends Component {
 
-  render() {
-    return (
-      <nav>
-      </nav>
-    )
-  }
+    renderNavItems = () => {
+        let elements = this.props.navItems.map((item, index) => {
+            return ( 
+                <a key={`nav-${index}`}
+                    onClick={ () => this.props.handleNavClick(item.value) } 
+                    href="#">
+                    { item.label }&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                </a>
+            )
+        })
+        return elements
+    }
+
+    render() {
+        return (
+            <nav>
+                { this.renderNavItems() }
+            </nav>
+        )
+    }
 }
 
 export default AppNav;
