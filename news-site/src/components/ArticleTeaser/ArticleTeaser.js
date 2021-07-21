a174c52291747c3c11f157ed8065cb1aac64ccf0
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-21 13:14:55 -0400

    class Teaser

--- news-site/src/components/ArticleTeaser/ArticleTeaser.js
+++ news-site/src/components/ArticleTeaser/ArticleTeaser.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
 class ArticleTeaser extends Component {
+
+  handleClick = () => {
+    this.props.handleTitleClick(this.props.id)
+  }
+
   render() {
     return (
       <div>
+        <div className='link'>
+          <a href='#' onClick={ this.handleClick }>
+            {this.props.title}
+          </a>
+          <p>
+            { this.props.created_date }
+          </p>
+        </div>
       </div>
     )
   }
 }
 
 export default ArticleTeaser;
+
+// The `ArticleTeaser`
+// component should:
+//   1. Display the `title`
+// inside of an `<a>`
+// tag.
+// 2. When the `title`
+// `<a>`
+// tag is clicked, it should call `this.props.handleTitleClick(this.props.id);`.Will arrow functions be useful here ?
+//   3. Display the `created_date` in a `<p>`
+// tag.
