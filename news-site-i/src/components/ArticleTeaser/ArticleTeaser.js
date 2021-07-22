f169e0bfb519284666457829294702dabe281d88
Jonas Paulikas <paulikas78@gmail.com>
2021-07-21 23:46:34 -0500

    news site I work in progress

--- news-site-i/src/components/ArticleTeaser/ArticleTeaser.js
+++ news-site-i/src/components/ArticleTeaser/ArticleTeaser.js
@@ -4,6 +4,8 @@ class ArticleTeaser extends Component {
   render() {
     return (
       <div>
+        <a onClick={ () => this.props.handleTitleClick(this.props.id) } href="#">{ this.props.title }</a>
+        <p>{ this.props.created_date }</p>
       </div>
     )
   }
