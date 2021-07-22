583c0e9c294412b5659f858576f85df60cf027b9
AlexanderS68 <80540025+AlexanderS68@users.noreply.github.com>
2021-07-22 01:00:31 -0400

    finished

--- news-site-i/src/components/AppNav/AppNav.js
+++ news-site-i/src/components/AppNav/AppNav.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class AppNav extends Component {
 
+  renderNavItems = () => {
+    let elements = this.props.navItems.map((item, index) => {
+      return (
+        <a onClick={ () => this.props.handleNavClick(item.value) } href='#'>
+          { item.label } 
+        </a>   
+      )
+    })
+    return elements
+  }
+
   render() {
     return (
       <nav>
+        { this.renderNavItems() }
       </nav>
     )
   }
